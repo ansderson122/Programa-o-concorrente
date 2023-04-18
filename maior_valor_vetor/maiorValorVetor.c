30a244ca0168c0867708e223177496a58ea1fb1d
JULYANDERSON CAVALCANTI DE LIMA LIMA <ansderson122@etiopia.k05s>
2023-04-18 09:16:38 -0300

    maior elemento

---
+++ maior_valor_vetor/maiorValorVetor.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <omp.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+
+#define MAX 100
+
+
+
+int *gerar_vetor_inteiro(int n);
+
+
+ 
+void main(){
+    long int tamanhoVetor = pow(10,9);
+    time_t t;
+    srand(time(NULL));
+    int *vetor = NULL;
+    int x = 3;
+    vetor = gerar_vetor_inteiro(tamanhoVetor);
+    double tempo_paralelo, tempo_serial, inicio,fim;
+    
+    int maiorElemento;
+    long int indexMaiorElemento;
+    // incio do SERIAL ------------------------------------------
+    printf("incio da serial \n");
+    inicio = omp_get_wtime();
+
+    
+    maiorElemento = vetor[0]; 
+    for(long int i = 0; i < tamanhoVetor; i++){
+        if(vetor[i] > maiorElemento){
+            indexMaiorElemento = i;
+            maiorElemento = vetor[i];
+        }
+    }
+    
+
+    fim = omp_get_wtime();
+    tempo_serial = fim - inicio;
+    printf("o maior elemento: %i \n",maiorElemento);
+    printf("o index do maior elemento: %i \n",indexMaiorElemento);
+    printf("Tempo  %f \n\n",tempo_serial);
+
+
+
+    // fim do SERIAL ------------------------------------------
+
+  
+
+
+    // inicio do paralelo ----------------------------------------
+    
+    printf("inicio do paralelo \n");
+     indexMaiorElemento = 0;
+    inicio = omp_get_wtime();
+
+    #pragma omp parallel num_threads(4)
+        {
+            maiorElemento = vetor[0]; 
+            #pragma omp for
+            for(long int i = 0; i < tamanhoVetor; i++){
+                if(vetor[i] > maiorElemento){
+                     #pragma omp critical
+                        indexMaiorElemento = i;
+                        maiorElemento = vetor[i];
+                }
+            }
+        } 
+
+
+
+    fim = omp_get_wtime();
+    tempo_paralelo = fim - inicio;
+    printf("o maior elemento: %i \n",maiorElemento);
+    printf("o index do maior elemento: %i \n",indexMaiorElemento);
+    printf("Tempo  %f \n\n",tempo_paralelo);
+        
+    // fim do paralelo ----------------------------------------
+
+
+
+    printf("Execução serial: %.5f \n",tempo_serial);
+    printf("Execução paralela: %.5f \n",tempo_paralelo);
+    double speedup = tempo_serial/tempo_paralelo;
+	printf("Speedup: %.4f\n", speedup);
+	printf("Eficiencia: %.4f\n\n",speedup/4.0);
+    
+
+
+}
+
+
+int *gerar_vetor_inteiro(int n) {
+    int *vetor;
+    long int i;
+    vetor = (int *)malloc(sizeof(int) * n);
+    for (i=0;i<n;i++) {
+        int num = (rand());
+        vetor[i] = num;
+    }
+    return vetor;
+}
